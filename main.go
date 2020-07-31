b7f31e1fc0b93d41f213ed4e8f6b9283cc8d8960
Justin Clift <justin@postgresql.org>
2020-07-31 18:10:43 +1000

    Added call to Columns(), for retrieving column info of a table or view

--- main.go
+++ main.go
@@ -68,4 +68,23 @@ func main() {
 		fmt.Printf("  * '%s' on table '%s'\n", i, j)
 	}
 	fmt.Println()
+
+	// Retrieve the column info for a table or view in the remote database
+	table := "table1"
+	columns, err := db.Columns("justinclift", "Join Testing.sqlite", table)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Columns on table or view '%s':\n", table)
+	for _, j := range columns {
+		fmt.Printf("  * '%v':\n", j.Name)
+		fmt.Printf("      Autoincrement: %v\n", j.Autoinc)
+		fmt.Printf("      Cid: %v\n", j.Cid)
+		fmt.Printf("      Collation Sequence: %v\n", j.CollSeq)
+		fmt.Printf("      Data Type: %v\n", j.DataType)
+		fmt.Printf("      Default Value: %v\n", j.DfltValue)
+		fmt.Printf("      Not Null: %v\n", j.NotNull)
+		fmt.Printf("      Primary Key: %v\n", j.Pk)
+	}
+	fmt.Println()
 }
